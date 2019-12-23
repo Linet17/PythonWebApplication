44506660ffaaecc145c974b1cd1dae7caded1113
Linet17 <linet.nyamu@gmail.com>
2019-12-23 09:52:52 +0300

    'sales'

--- models/sales.py
+++ models/sales.py
@@ -5,6 +5,12 @@ class SalesModel(db.Model):
     __tablename__ = 'sales'
     # __table_args__ = {'extend_existing': True}
     # to add columns,use your sqlalchemy object to get the column class that'll help you define your columns
-    id = db.Column(db.Integer, primary_key=True)  # research on additional objects you can pass in the column id
-    quantity = db.Column(db.Integer, nullable=False)
+    # id = db.Column(db.Integer, primary_key=True)
+    # # research on additional objects you can pass in the column id
+    # quantity = db.Column(db.Integer, nullable=False)
+
+    id = db.Column(db.Integer,primary_key=True)
     inv_id = db.Column(db.Integer, db.ForeignKey('inventories.id'))
+    inv_name = db.Column(db.String, nullable=False)
+    inv_type = db.Column(db.String, nullable=False)
+
